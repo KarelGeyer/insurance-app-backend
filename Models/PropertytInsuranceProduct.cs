d157ecc6f4d2b09f6e403c988b9d1664761786de
Karel Geyer <karelgeyer@gmail.com>
2024-01-30 18:58:31 +0100

    change the file and class name to correct name

--- Models/PropertytInsuranceProduct.cs
+++ Models/PropertytInsuranceProduct.cs
@@ -3,7 +3,7 @@ using MongoDB.Bson;
 
 namespace insurance_backend.Models
 {
-	public class ProductInsuranceProduct
+	public class PropertytInsuranceProduct
 	{
 		[BsonId]
 		[BsonRepresentation(BsonType.ObjectId)]
